c4ec722674b166db513fa897f90b8dfcf20603fa
sharomn <158808362+sharomn@users.noreply.github.com>
2025-08-18 00:18:08 +0400

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -20,7 +20,7 @@ CREATE TABLE Books (
 );
 
 -- Create CUSTOMERS table
-CREATE TABLE CUSTOMERS (
+CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) UNIQUE,
@@ -28,25 +28,25 @@ CREATE TABLE CUSTOMERS (
 );
 
 -- Create ORDERS table
-CREATE TABLE ORDERS (
+CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE,
-    FOREIGN KEY (customer_id) REFERENCES CUSTOMERS(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
 
 -- Create ORDER_DETAILS table
-CREATE TABLE ORDER_DETAILS (
+CREATE TABLE Order_details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
