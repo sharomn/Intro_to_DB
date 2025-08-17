4dac1fcb4290a4a3344c58a30482edb5f445906f
sharomn <158808362+sharomn@users.noreply.github.com>
2025-08-17 21:58:18 +0400

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -4,8 +4,8 @@ USE alx_book_store;
 
 -- Authors table
 CREATE TABLE AUTHORS (
-    AUTHOR_ID INT AUTO_INCREMENT PRIMARY KEY,
-    AUTHOR_NAME VARCHAR(215) NOT NULL
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
 );
 
 -- Books table
